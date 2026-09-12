34e821564e9ca2580c14626e159f543dcd32924d
developerthierry <developerthierry@gmail.com>
2026-09-12 00:52:04 +0000

    Connect OMDb API and routees

--- movies-backend/index.js
+++ movies-backend/index.js
@@ -1,15 +1,18 @@
+import "dotenv/config" //process env state
 // our server config
 import express from "express" //import express
 const app = express() // express instane, can define routes, middleware, etc
-
 const port  = 3001
-
+const apiKey = process.env.API_KEY
 // one of the http verbs , fetching data from our resource
 app.get("/", (req, res) => {
     res.send("movie app is running")
 })
 
-app.get("/getMovies", (req, res) => {
+app.get("/getMovies", async (req, res) => {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&s=mario`)
+    const data = await response.json()
+    console.log(data)
     res.send("Hey going to fetch movies for you")
 })
 
