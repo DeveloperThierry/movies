b1611e7045a97860a3f161811dbd626df9ac7b53
developerthierry <developerthierry@gmail.com>
2026-09-12 01:22:35 +0000

    Init frontend

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -2,14 +2,14 @@
 # see: https://firebase.google.com/docs/studio/customize-workspace
 { pkgs, ... }: {
   # Which nixpkgs channel to use.
-  channel = "stable-24.05"; # or "unstable"
+  channel = "unstable";
 
   # Use https://search.nixos.org/packages to find packages
   packages = [
     # pkgs.go
     # pkgs.python311
     # pkgs.python311Packages.pip
-    pkgs.nodejs_20
+    pkgs.nodejs_latest
     # pkgs.nodePackages.nodemon
   ];
 
